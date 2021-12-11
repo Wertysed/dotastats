c188654abd93b3db3833333c42bbf25844704566
Wertysed <wertysed11@gmail.com>
2021-12-11 06:29:13 +0500

    full functionality

--- File_1.py
+++ File_1.py
@@ -1,9 +1,9 @@
 import requests
 import discord
 from discord.ext import commands
-from config import settings
+import os
 
-bot = commands.Bot(command_prefix=settings['prefix'])
+bot = commands.Bot(command_prefix=os.environ['PREFIX'])
 bot.remove_command('help')
 
 
@@ -49,18 +49,23 @@ class DotaAPI:
 
     @classmethod
     def information_output(cls, request_api, way):
+        inf = []
         if way == 1:  # словарь в списке
             for i in request_api:
                 for key, value in i.items():
-                    print(key, ":", value)
+                    pre_inf = f'{key} : {value}'
+                    inf.append(pre_inf)
         elif way == 2:  # словарь
             for key, value in request_api.items():
                 if isinstance(value, dict):
-                    print(key)
+                    inf.append(key)
                     for key_1, value_2 in value.items():
-                        print(key_1, ":", value_2)
+                        pre_inf = f'{key_1} : {value_2}'
+                        inf.append(pre_inf)
                 else:
-                    print(key, ':', value)
+                    pre_inf = f'{key} : {value}'
+                    inf.append(pre_inf)
+        return '\n'.join(inf)
 
     @classmethod
     def stats_of_player(cls, id_of_player):
@@ -91,13 +96,11 @@ class DotaAPI:
         url = f'{cls.BASE_URL}players/{id_of_player}/wordcloud'
         abobus = requests.get(url).json()
         if name_of_hero == 'all':
-            print(abobus)
             return cls.information_output(abobus, 2)
         else:
             id_of_hero = cls.search_id_of_hero(name_of_hero)
             url_new = f'{url}?hero_id={id_of_hero}'
             abobus_new = requests.get(url_new).json()
-            print(abobus_new)
             return cls.information_output(abobus_new, 2)
 
     @classmethod
@@ -112,10 +115,13 @@ class DotaAPI:
         id_of_team = cls.search_id_of_team(name_of_team)
         url = f'{cls.BASE_URL}teams/{id_of_team}/players'
         abobus = requests.get(url).json()
+        inf = []
         for i in abobus:
             if i['is_current_team_member']:
                 for key, value in i.items():
-                    print(key, ':', value)
+                    pre_inf = f'{key}:{value}'
+                    inf.append(pre_inf)
+        return '\n'.join(inf)
 
     @classmethod
     def stats_of_all_team_hero(cls, name_of_team, name_of_hero):
@@ -125,11 +131,11 @@ class DotaAPI:
         abobus = requests.get(url).json()
         for i in abobus:
             if i['hero_id'] == id_of_hero:
-                cls.information_output(i, 2)
+                return cls.information_output(i, 2)
 
 
 @bot.command()
-async def st_for_pl(ctx, argument):
+async def player(ctx, argument):
     argument = int(argument)
     await ctx.send(DotaAPI.stats_of_player(argument))
 
@@ -146,46 +152,53 @@ async def wl(ctx, argument_1, *, argument_2):
     argument_1 = int(argument_1)
     await ctx.send(DotaAPI.wl_of_player(argument_1, argument_2))
 
+
 @bot.command()
 async def friends(ctx, argument):
     argument = int(argument)
     await ctx.send(DotaAPI.stats_with_peers(argument))
+
 
 @bot.command()
 async def wordcloud(ctx, argument_1, *, argument_2):
     argument_1 = int(argument_1)
     await ctx.send(DotaAPI.stats_of_word(argument_1, argument_2))
 
+
 @bot.command()
 async def team(ctx, *, argument):
     await ctx.send(DotaAPI.stats_of_team(argument))
+
 
 @bot.command()
 async def team_player(ctx, *, argument):
     await ctx.send(DotaAPI.stats_of_all_team_players(argument))
 
+
 @bot.command()
 async def team_hero(ctx, argument_1, *, argument_2):
     argument_1 = argument_1.replace('_', ' ')
-    await ctx.send(DotaAPI.stats_of_all_team_hero(argument_1 , argument_2))
+    await ctx.send(DotaAPI.stats_of_all_team_hero(argument_1, argument_2))
 
 
-bot.run(settings['token'])
+@bot.command()
+async def help(ctx):
+    emd = discord.Embed(title='Command navigation')
+    emd.add_field(name='{}player'.format(os.environ['PREFIX']), value='Get player stats')
+    emd.add_field(name='{}record'.format(os.environ['PREFIX']), value='Get interesting record. You can choose for '
+                                                                      'each hero separately. Only possible: duration,'
+                                                                      ' kills, deaths, assists.')
+    emd.add_field(name='{}wl'.format(os.environ['PREFIX']), value='Get wl stats. You can choose for each hero '
+                                                                  'separately.')
+    emd.add_field(name='{}friends'.format(os.environ['PREFIX']), value='not working at the moment')
+    emd.add_field(name='{}wordcloud'.format(os.environ['PREFIX']), value='Сhat your games. You can choose for each '
+                                                                         'hero separately.')
+    emd.add_field(name='{}team'.format(os.environ['PREFIX']), value='Get team stats')
+    emd.add_field(name='{}team_player'.format(os.environ['PREFIX']), value='Get information about the teams players')
+    emd.add_field(name='{}team_hero'.format(os.environ['PREFIX']),
+                  value='Get wl stats on any hero that the team played')
+
+    await ctx.send(embed=emd)
 
 
-
-
-
-
-# print('пример рекорда', DotaAPI.record_search(339665220, 'kills', 'all'))  # внешний функционал
-# print('пример поиска id команды по названию', DotaAPI.search_id_of_team('PSG.LGD'))  # внутренний функционал
-# print('пример получения id по названию героя', DotaAPI.search_id_of_hero('Slark'))  # внутренний функционал
-# print('пример получения статы игрока', DotaAPI.stats_of_player(339665220))  # внешний функционал
-# print('пример получения статы побед и поражений', DotaAPI.wl_of_player(339665220, 'Slark'))  # внешний функционал
-# print('пример статы с друзьями', DotaAPI.stats_with_peers(1084744980))  # внешний функционал
-# print('пример вывода ласт слов', DotaAPI.stats_of_word(1084744980, 'Luna'))  # внешний функционал
-# print('пример получения статы про команд', DotaAPI.stats_of_team('Team Spirit'))  # внешний функционал
-# print('пример получения статы для каждого игорка(играющего)', DotaAPI.stats_of_all_team_players('PSG.LGD'))  #
-# внешний функционал
-# print('пример получения статы с определнным героем', DotaAPI.stats_of_all_team_hero('Team Spirit', 'Magnus'))  #
-# внешний функционал
+bot.run(os.environ['DISCORD_TOKEN'])
